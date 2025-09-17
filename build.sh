61b7d7101950670c4f678c462371c7df27e4c243
drehuwann <drehuwann@gmail.com>
2025-09-18 04:07:57 +1100

    Refactor Bochs config and build script logic
    Replace static .bochsrc with symlink based on GDB support.
    Update build.sh to modify .vscode/tasks.json dynamically.
    Create .bochsrc symlink depending on Bochs GDB capability.
    Clean up tasks and improve CMake flags for consistency.

--- build.sh
+++ build.sh
@@ -14,3 +14,32 @@ pushd build
     dd if=bootloader.bin of=../output/knkOs64.img conv=notrunc
     dd if=kern.bin of=../output/knkOs64.img bs=512 seek=1 conv=notrunc
 popd
+
+###########
+#   config environment nefore run
+###########
+##sed patterns
+mark="-qf .bochsrc"
+switchOnOff="-rc debug.rc"
+file2feedSed="./.vscode/tasks.json" 
+target2link2=".bochsrc"
+# 
+#####################################33
+#   check if bochs is available
+BOCHS=$(which bochs)
+if [[ -n $BOCHS ]]; then
+    # Frees the old symbolic sync `.bochsrc`
+    rm .bochsrc
+    # Check if $BOCHS was built with gdbstub    
+    if [[ $(readelf -p .dynstr $BOCHS | grep gdbstub | wc -l) -ne 0 ]]; then
+        # Remove the switch from tasks.json
+        sed -i "s/$switch[[:space:]]*//g" "$tasksJson"
+        # Hook the symbolic link to the GDB_specific `.bochsrc`
+        ln -s .bochsrcGDB_noSMP $target2link2
+    else
+        # gdbstub is not present : Add the switch into tasks.sjon
+        sed -i "/$pattern/!{h;d}; /$text/!{s/$pattern/$pattern $text/;}" "$file2feedSed"
+        # Hook the symbolic link to the SMP_specific `.bochsrc`
+        ln -s .bochsrcSMP_noGDB $target2link2
+    fi
+fi
