3accc77e3cc4cf46a78bd65154e3ad74f68eefb6
drehuwann <drehuwann@gmail.com>
2025-09-18 13:00:07 +1100

    feat(gdb): gdbstub/automate dbg symbol workflow
    - enable gdbstub in .bochsrcGDB_noSMP at port 1234 with bases set to 0
    - add objcopy commands in CMakeLists to extract DWARF and strip symbols
    - update build.sh to generate kern.bin with embedded debuglink
    - reintegrate kern.bin into the disk image via dd

--- build.sh
+++ build.sh
@@ -9,7 +9,12 @@ pushd build
     nasm ../src/sector2+/extended.asm -f elf64 -i ../src/sector2+ -o extended.o
     nasm ../src/sector2+/binaries.asm -f elf64 -i ../src/sector2+ -o binaries.o
     cmake ../CMakeLists.txt -G "Unix Makefiles" -D CMAKE_C_COMPILER=x86_64-elf-gcc
-    make -f "Makefile"
+    make
+    x86_64-elf-ld -T link.ld -o kern.elf libkernel.a extended.o binaries.o
+    x86_64-elf-objcopy --only-keep-debug kern.elf kern.sym
+    x86_64-elf-objcopy --strip-debug kern.elf kern.bin
+    x86_64-elf-objcopy --add-gnu-debuglink=kern.sym kern.bin
+
     dd if=/dev/zero of=../output/knkOs64.img count=1008 bs=512
     dd if=bootloader.bin of=../output/knkOs64.img conv=notrunc
     dd if=kern.bin of=../output/knkOs64.img bs=512 seek=1 conv=notrunc
